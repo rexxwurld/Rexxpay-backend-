8818fe5e51905101528215f043243e8b1bbf8061
Emmanuel Edidiong Noah <rexxwurld@gmail.com>
2026-06-28 06:47:08 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-const cookieParser = require("cookie-parser");
+
 
 const app = express();
 
 // 🔥 CORE MIDDLEWARE
 app.use(express.json());
-app.use(cookieParser());
+
 
 // 🔥 CORS (COOKIE SAFE)
 app.use(cors({
@@ -14,8 +14,7 @@ app.use(cors({
         "http://127.0.0.1:5500",
         "http://localhost:5500",
         "https://rexxpay.netlify.app"
-    ],
-    credentials: true
+    ]
 }));
 
 // ROUTES
