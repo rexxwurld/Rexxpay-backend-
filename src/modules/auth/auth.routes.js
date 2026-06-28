cb069dcc7a993a8dc0e167b0b9878176576985ef
Emmanuel Edidiong Noah <rexxwurld@gmail.com>
2026-06-28 03:55:26 +0100

    Add logout route to auth.routes.js

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -3,5 +3,6 @@ const controller = require("./auth.controller");
 
 router.post("/register", controller.register);
 router.post("/login", controller.login);
+router.post("/logout", controller.logout);
 
 module.exports = router;
